3d2edf3c4b1322a8e64c37c803796d30fc7b57d3
Sorokin Iury <144129825+akiraduck@users.noreply.github.com>
2024-04-18 14:32:09 +0500

    Первый апдейт
    Основные файлы с банально логикой движения персонажа

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace shooterGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAmmo = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtHealth = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Timers.Timer();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameTimer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAmmo
+            // 
+            this.txtAmmo.Font = new System.Drawing.Font("Comic Sans MS", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtAmmo.ForeColor = System.Drawing.Color.Red;
+            this.txtAmmo.Location = new System.Drawing.Point(12, 23);
+            this.txtAmmo.Name = "txtAmmo";
+            this.txtAmmo.Size = new System.Drawing.Size(213, 74);
+            this.txtAmmo.TabIndex = 0;
+            this.txtAmmo.Text = "Пули: 0";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Comic Sans MS", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtScore.ForeColor = System.Drawing.Color.Red;
+            this.txtScore.Location = new System.Drawing.Point(222, 23);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(277, 72);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Убийства: 0";
+            this.txtScore.Click += new System.EventHandler(this.txtScore_Click);
+            // 
+            // txtHealth
+            // 
+            this.txtHealth.Font = new System.Drawing.Font("Comic Sans MS", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtHealth.ForeColor = System.Drawing.Color.Red;
+            this.txtHealth.Location = new System.Drawing.Point(495, 23);
+            this.txtHealth.Name = "txtHealth";
+            this.txtHealth.Size = new System.Drawing.Size(230, 66);
+            this.txtHealth.TabIndex = 2;
+            this.txtHealth.Text = "Здоровье:";
+            // 
+            // healthBar
+            // 
+            this.healthBar.Location = new System.Drawing.Point(731, 23);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(394, 66);
+            this.healthBar.TabIndex = 3;
+            this.healthBar.Value = 100;
+            // 
+            // player
+            // 
+            this.player.Image = global::shooterGame.Properties.Resources.up;
+            this.player.Location = new System.Drawing.Point(521, 489);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(32, 32);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            this.player.Click += new System.EventHandler(this.player_Click);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20D;
+            this.GameTimer.SynchronizingObject = this;
+            this.GameTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.MainTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 40F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1174, 629);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.txtHealth);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtAmmo);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Ultrakill";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameTimer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Timers.Timer GameTimer;
+
+        private System.Windows.Forms.PictureBox player;
+
+        private System.Windows.Forms.ProgressBar healthBar;
+
+        private System.Windows.Forms.Label txtHealth;
+
+        private System.Windows.Forms.Label txtScore;
+
+        private System.Windows.Forms.Label txtAmmo;
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using System.Windows.Forms;
+using shooterGame.Properties;
+
+namespace shooterGame
+{
+    public partial class Form1 : Form
+    {
+        private bool goUp, goDown, goRight, goLeft, gameOver;
+        private string facing = "up";
+        private int playerHealth = 100;
+        private int speed = 10;
+        private int ammo = 10;
+        private int score;
+        private int enemiesSpeed = 3;
+        private Random randomNumber = new Random();
+        private List<PictureBox> enemiesList = new List<PictureBox>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void player_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MainTimerEvent(object sender, ElapsedEventArgs e)
+        {
+            if (playerHealth > 1)
+            {
+                healthBar.Value = playerHealth;
+            }
+
+            else
+            {
+                gameOver = true;
+            }
+
+            txtAmmo.Text = "Пули: " + ammo;
+            txtScore.Text = "Убийства: " + score;
+
+            if (goLeft && player.Left > 0)
+            {
+                player.Left -= speed;
+            }
+
+            if (goRight && player.Left + player.Width < this.ClientSize.Width)
+            {
+                player.Left += speed;
+            }
+            
+            if (goUp && player.Top > 92)
+            {
+                player.Top -= speed;
+            }
+            
+            if (goDown && player.Top + player.Height < this.ClientSize.Height)
+            {
+                player.Top += speed;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                goLeft = false;
+            }
+                        
+            if (e.KeyCode == Keys.D)
+            {
+                goRight = false;
+            }
+                        
+            if (e.KeyCode == Keys.W)
+            {
+                goUp = false;
+            }
+                        
+            if (e.KeyCode == Keys.S)
+            {
+                goDown = false;
+            }
+            
+            if (e.KeyCode == Keys.Space && ammo > 0)
+            {
+                ammo--;
+                ShootBullet(facing);
+
+                if (ammo < 1)
+                {
+                    DropAmmo();
+                }
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                goLeft = true;
+                facing = "left";
+                player.Image = Resources.left;
+            }
+            
+            if (e.KeyCode == Keys.D)
+            {
+                goRight = true;
+                facing = "right";
+                player.Image = Resources.right;
+            }
+            
+            if (e.KeyCode == Keys.W)
+            {
+                goUp = true;
+                facing = "up";
+                player.Image = Resources.up;
+            }
+            
+            if (e.KeyCode == Keys.S)
+            {
+                goDown = true;
+                facing = "down";
+                player.Image = Resources.down;
+            }
+        }
+
+        private void ShootBullet(string direction)
+        {
+            var shootBullet = new Bullet();
+            shootBullet.direction = direction;
+            shootBullet.bulletLeft = player.Left + (player.Width / 2);
+            shootBullet.bulletTop = player.Top + (player.Height / 2);
+            shootBullet.MakeBullet(this);
+        }
+
+        private void MakeEnemies()
+        {
+            var enemy = new PictureBox();
+            enemy.Tag = "enemy";
+            enemy.Image = Resources.enemy;
+            enemy.Left = randomNumber.Next(0, 1200);
+            enemy.Top = randomNumber.Next(92, 700);
+            enemy.SizeMode = PictureBoxSizeMode.AutoSize;
+            enemiesList.Add(enemy);
+            Controls.Add(enemy);
+            player.BringToFront();
+        }
+
+        private void DropAmmo()
+        {
+            var ammo = new PictureBox();
+            ammo.Image = Resources.bullet;
+            ammo.SizeMode = PictureBoxSizeMode.Normal;
+            ammo.Left = randomNumber.Next(10, ClientSize.Width - ammo.Width);
+            ammo.Top = randomNumber.Next(82, ClientSize.Height - ammo.Height);
+            ammo.Tag = "ammo";
+            Controls.Add(ammo);
+            
+            ammo.BringToFront();
+            player.BringToFront();
+        }
+
+        private void RestartGame()
+        {
+            
+        }
+
+        private void txtScore_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
